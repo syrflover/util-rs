032ba4a6e5af5e7b0fd6e6627700eb72bc187c3a
Lee TaeWoo <syr@meu.works>
2023-02-10 20:38:49 +0900

    update sea-orm to 0.11

--- sea-orm-util/src/order_by.rs
+++ sea-orm-util/src/order_by.rs
@@ -1,7 +1,5 @@
-use std::sync::Arc;
-
 use sea_orm::{
-    sea_query::{Function, SimpleExpr},
+    sea_query::{Func, SimpleExpr},
     EntityTrait, Order, QueryOrder, Select,
 };
 
@@ -14,7 +12,8 @@ pub trait OrderByRandom {
 impl<E: EntityTrait> OrderByRandom for Select<E> {
     fn order_by_random(mut self) -> Self {
         QueryOrder::query(&mut self).order_by_expr(
-            SimpleExpr::FunctionCall(Function::Custom(Arc::new(Random)), Vec::new()),
+            // SimpleExpr::FunctionCall(Function::Custom(Arc::new(Random)), Vec::new()), // 0.9.x
+            SimpleExpr::FunctionCall(Func::cust(Random)), // 0.11.x
             Order::Desc,
         );
 
