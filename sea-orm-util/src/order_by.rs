31577367ae9a0e7e2de72cc13b9966edb58d8978
syr <user@syrflover.co>
2024-02-03 18:53:23 +0900

    feat(sea-orm): add implementation of `OrderByRandom`

--- sea-orm-util/src/order_by.rs
+++ sea-orm-util/src/order_by.rs
@@ -1,5 +1,5 @@
 use sea_orm::{
-    sea_query::{Func, SimpleExpr},
+    sea_query::{Func, SelectStatement, SimpleExpr},
     EntityTrait, Order, QueryOrder, Select,
 };
 
@@ -12,6 +12,18 @@ pub trait OrderByRandom {
 impl<E: EntityTrait> OrderByRandom for Select<E> {
     fn order_by_random(mut self) -> Self {
         QueryOrder::query(&mut self).order_by_expr(
+            // SimpleExpr::FunctionCall(Function::Custom(Arc::new(Random)), Vec::new()), // 0.9.x
+            SimpleExpr::FunctionCall(Func::cust(Random)), // 0.11.x
+            Order::Desc,
+        );
+
+        self
+    }
+}
+
+impl OrderByRandom for SelectStatement {
+    fn order_by_random(mut self) -> Self {
+        self.order_by_expr(
             // SimpleExpr::FunctionCall(Function::Custom(Arc::new(Random)), Vec::new()), // 0.9.x
             SimpleExpr::FunctionCall(Func::cust(Random)), // 0.11.x
             Order::Desc,
